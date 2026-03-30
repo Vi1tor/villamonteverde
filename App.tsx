a7f0a5254f2b4b9b6580686bbd001affed2075d6
Vi1tor <vitor7pb@gmail.com>
2026-03-30 19:56:05 -0300

    wip

--- App.tsx
+++ App.tsx
@@ -522,7 +522,7 @@ const App: React.FC = () => {
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"/>
               </svg>
-              Ir para Villa Barril (Navegação)
+              Ir para Villa Monte Verde (Navegação)
             </a>
           </div>
         </div>
